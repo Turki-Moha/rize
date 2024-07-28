155b229ea5fe8aadb98318c3989dadb727962d14
Turki Alkazman <turkimohsen33@gmail.com>
2024-07-29 02:49:28 +0300

    updated: - fixed button with appropriate colors

--- components/navigationBar.tsx
+++ components/navigationBar.tsx
@@ -1,13 +1,13 @@
 "use client"
 import React, { useState } from 'react';
 import { Home, Search, MessageSquare, Grid, Plus } from 'lucide-react';
-
+import Image from 'next/image';
 const BottomNav = () => {
     const tab = ["Home", "Search", "Chat", "Menu"]
-    const tabIcon = [<Home />, <Search />, <MessageSquare size={24} />, <Grid size={24} />]
+    const tabIcon = [<Home />, <Search />, <MessageSquare size={36} />, <Grid size={36} />]
     const [isActive, setIsActive] = useState(0);
     return (
-        <nav className="absolute bottom-0 left-0 right-0 p-4" >
+        <nav className="absolute bottom-0 left-0 right-0 p-4 bg-white-750" >
 
             <div className="flex justify-between items-center max-w-screen-sm mx-auto relative">
                 {tab.map((item, index) => {
@@ -23,8 +23,8 @@ const BottomNav = () => {
                 })}
                 {/* Floating Action Button */}
                 <div className="absolute left-1/2 -translate-x-1/2 -top-8">
-                    <button className="bg-cyan-300 text-white-850 p-4 rounded-full shadow-lg hover:bg-cyan-300 transition-colors duration-300">
-                        <Plus size={24} />
+                    <button className="bg-cyan-300 text-white-850 p-4 rounded-full shadow-lg transition-colors duration-300 w-16 h-16">
+                        {<Image className="m-auto" src="/logo.png" width={24} height={24} alt='' />}
                     </button>
                 </div>
             </div>
@@ -36,11 +36,13 @@ const NavItem = ({ icon, label, isActive = false, onClick }: {
     icon: any, label: string, isActive?: boolean, onClick?: () => void
 }) => {
     return (
-        <button className={`flex flex-col items-center ${isActive ? 'text-cyan-300' : ''}`} onClick={onClick}>
-            <div className={`p-2 rounded-full ${isActive ? 'bg-cyan-300 bg-opacity-20' : ''}`}>
-                {icon}
+        <button className={`flex flex-col items-center ${isActive ? 'text-white-750' : 'text-white-500'}`} onClick={onClick}>
+            <div className={`p-4 rounded-full ${isActive ? 'bg-cyan-300' : 'text-white-500'}`}>
+                <div>
+                    {icon}
+                </div>
             </div>
-            <span className="text-xs mt-1">{label}</span>
+            <span className={`text-xs ${isActive ? 'opacity-0' : ''}`}>{label}</span>
         </button>
     );
 };
